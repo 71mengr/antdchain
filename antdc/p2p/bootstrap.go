c917e5736784c545c74571f965da416195708ff6
G.E.M <doo@ghash.xyz>
2026-05-11 15:23:52 +0000

    Added properly peer ID

--- antdc/p2p/bootstrap.go
+++ antdc/p2p/bootstrap.go
@@ -5,7 +5,7 @@ import "strings"
 // DefaultBootstrapPeers contains static seed nodes for initial peer discovery.
 // Add reliable, publicly reachable peers here in multiaddr format.
 var DefaultBootstrapPeers = []string{
-	"/ip4/129.151.164.202/tcp/3000/p2p/12D3KooWDDLZ3Z6xLS22DcoE98ZQggXgi2uMdb4q9sqFQ8WBPJd4",
+	"/ip4/129.151.164.202/tcp/3000/p2p/12D3KooWLTXBBrRL7HZG6JwUgpdVvfDW4BmJNokwmp8KZv2E7L7a",
 }
 
 // ResolveBootstrapPeers merges configured peers with default peers,
